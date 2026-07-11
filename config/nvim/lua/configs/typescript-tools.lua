70a11ecbe4e1f35838549ddf1b6ee102e1927025
not-manu <manu.anish@mail.utoronto.ca>
2026-07-11 22:02:53 +0530

    nvim: cap memory usage for tsserver

--- config/nvim/lua/configs/typescript-tools.lua
+++ config/nvim/lua/configs/typescript-tools.lua
@@ -26,7 +26,7 @@ local options = {
     tsserver_plugins = {},
     -- this value is passed to: https://nodejs.org/api/cli.html#--max-old-space-sizesize-in-megabytes
     -- memory limit in megabytes or "auto"(basically no limit)
-    tsserver_max_memory = "auto",
+    tsserver_max_memory = 2048,
     -- described below
     tsserver_format_options = {},
     tsserver_file_preferences = {
