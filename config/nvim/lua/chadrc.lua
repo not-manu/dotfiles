ee06342ebd9a58e382b81d6e54b3dce3e5f8fc8d
not-manu <manu.anish@mail.utoronto.ca>
2026-08-04 19:04:40 +0530

    nvim: better statusline

--- config/nvim/lua/chadrc.lua
+++ config/nvim/lua/chadrc.lua
@@ -1,17 +1,12 @@
--- This file needs to have same structure as nvconfig.lua 
--- https://github.com/NvChad/ui/blob/v3.0/lua/nvconfig.lua
--- Please read that file to know all available options :( 
-
 ---@type ChadrcConfig
 local M = {}
 
 M.base46 = {
 	theme = "flexoki-alt",
 
-	-- hl_override = {
-	-- 	Comment = { italic = true },
-	-- 	["@comment"] = { italic = true },
-	-- },
+	hl_override = {
+		St_Lsp = { fg = "light_grey" },
+	},
 }
 
 M.term = {
@@ -27,11 +22,11 @@ M.term = {
 
 M.ui = {
 	statusline = {
-		order = { "mode", "file", "git", "recording", "%=", "lsp_msg", "%=", "diagnostics", "lsp", "cwd", "cursor" },
+		order = { "mode", "file", "git", "recording", "%=", "lsp_msg", "%=", "diagnostics", "lsp" },
 		modules = {
 			recording = function()
 				if vim.g.recording_mode then
-					return "%#St_InsertMode#  REC %#St_InsertModeSep#"
+					return "%#St_lspError#  REC "
 				end
 				return ""
 			end,
